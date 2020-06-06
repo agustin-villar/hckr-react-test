14fd539bfc06420467026230ab2dd82b8dbb9ea3
Agustin Villar <amauro85@gmail.com>
2020-06-06 17:42:10 +0200

    add initial fetch

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-class Articles extends React.Component {
+const apiEndpoint = 'https://jsonmock.hackerrank.com/api/articles?author=';
 
-  render() {
-    return (
-      <React.Fragment>
-        <div className="controls">
-          <div className="input-container">
-            <span>author:</span>
-            <input type="text" className="text-input" data-testid="text-input" />
-            <button className="fetch-button" data-testid="fetch-button">Fetch</button>
-          </div>
-        </div>
-        <div className="results">
-          <li key="example-key" data-testid="result-row">Example title</li>
-        </div>
-        <div data-testid="no-results">No results</div>
-      </React.Fragment>
-    );
+const Articles = () => {
+  const [author, setAuthor] = useState('');
+  const [response, setResponse] = useState([]);
+
+  async function fetchData() {
+    let json;
+    let response;
+
+    try {
+      json = await fetch(`${apiEndpoint}${author}&page=1`);
+      response = await json.json();
+    } catch(e) {
+      response = e;
+    }
+    
+    console.log(response);
+    setResponse(response.data);
   }
-}
+
+  return (
+    <React.Fragment>
+      <div className="controls">
+        <div className="input-container">
+          <span>author:</span>
+          <input type="text" className="text-input" data-testid="text-input" onChange={(e) => setAuthor(e.target.value)} />
+          <button className="fetch-button" data-testid="fetch-button" onClick={fetchData}>Fetch</button>
+        </div>
+      </div>
+      <div className="results">
+        {response && response.map(({ title }) => (
+            <li key={title} data-testid="result-row">{title}</li>
+          ))}
+      </div>
+      {!response && (
+        <div data-testid="no-results">No results</div>
+      )}
+    </ React.Fragment>
+  );
+};
 
 export default Articles;
